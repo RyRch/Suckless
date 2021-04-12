68d684ac4982781e396cabd17b1c94554ebf2931
reda <reda.rouich@flexiworl.fr>
2021-04-13 01:50:32 +0200

    [ADD] tag rule discord

--- Dwm/config.h
+++ Dwm/config.h
@@ -59,8 +59,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1<<4,            0,           -1 },
+	{ "Gimp",     NULL,       NULL,       1<<3,            0,           -1 },
 	{ "Chromium", NULL,       NULL,       1<<1,            0,           -1 },
+    { "discord",  NULL,       NULL,       1<<2,            0,           -1 },
 };
 
 /* layout(s) */
